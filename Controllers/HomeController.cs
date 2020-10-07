1b0b2aa6860d4950a172a5b067d63bf696377235
Tim Tieng <51296279+TimTieng@users.noreply.github.com>
2020-10-07 11:53:01 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿/*
+Class: ISTA 322 - Developing Web Applications with ASP.NET CORE
+Student: Tim Tieng
+Instructor: Christine Lee
+Date: 26 August 2020
+Description: Sports Store MVC Application 
+Last Revised On: 23 September 2020
+Last Revised By: Tim Tieng
+Revisions Made: 
+    1. Added Seed Data - Ended on page 140 (31Aug2020)
+    2. Continued with Project (Added Pagination) 
+    3. Started implementing navigation controls
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+
+        public int PageSize = 4; //Added on 07SEP2020 - Pagination Feature
+        public HomeController(IStoreRepository repo) 
+        {
+            repository = repo;
+        }
+        //public IActionResult Index() => View(repository.Products);
+
+        //Index() Updated for pagination feature for select information to be published to View
+        public ViewResult Index(string category,int productPage = 1) //Updated on 23SEP to include Category parameter
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                .Where(p=> category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = category == null ?
+                    repository.Products.Count() :
+                    repository.Products.Where(e =>
+                        e.Category == category).Count()
+            },
+            CurrentCategory = category
+        });
+    }
+}
---
+++ Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿/*
+Class: ISTA 322 - Developing Web Applications with ASP.NET CORE
+Student: Tim Tieng
+Instructor: Christine Lee
+Date: 26 August 2020
+Description: Sports Store MVC Application 
+Last Revised On: 23 September 2020
+Last Revised By: Tim Tieng
+Revisions Made: 
+    1. Added Seed Data - Ended on page 140 (31Aug2020)
+    2. Continued with Project (Added Pagination) 
+    3. Started implementing navigation controls
+    4. Adding Shopping Cart Feature and Session States
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderRepository repository;
+        private Cart cart;
+        public OrderController(IOrderRepository repoService, Cart cartService)
+        {
+            repository = repoService;
+            cart = cartService;
+        }
+        public ViewResult Checkout() => View(new Order());
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                cart.Clear();
+                return RedirectToPage("/Completed", new { orderId = order.OrderID });
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
