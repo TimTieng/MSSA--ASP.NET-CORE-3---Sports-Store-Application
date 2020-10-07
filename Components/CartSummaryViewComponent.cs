02518ecb0b0f8c2d417de9af378d30773898e578
Tim Tieng <51296279+TimTieng@users.noreply.github.com>
2020-10-07 11:52:46 -0400

    Add files via upload

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,37 @@
+﻿/*
+Class: ISTA 322 - Developing Web Applications with ASP.NET CORE
+Student: Tim Tieng
+Instructor: Christine Lee
+Date: 26 August 2020
+Description: Sports Store MVC Application 
+Last Revised On: 23 September 2020
+Last Revised By: Tim Tieng
+Revisions Made: 
+    1. Added Seed Data - Ended on page 140 (31Aug2020)
+    2. Continued with Project (Added Pagination) 
+    3. Started implementing navigation controls
+    4. Adding Shopping Cart Feature and Session States
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService) 
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke() 
+        {
+            return View(cart);
+        }
+    }
+}
---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,39 @@
+﻿/*
+Class: ISTA 322 - Developing Web Applications with ASP.NET CORE
+Student: Tim Tieng
+Instructor: Christine Lee
+Date: 26 August 2020
+Description: Sports Store MVC Application 
+Last Revised On: 23 September 2020
+Last Revised By: Tim Tieng
+Revisions Made: 
+    1. Added Seed Data - Ended on page 140 (31Aug2020)
+    2. Continued with Project (Added Pagination) 
+    3. Started implementing navigation controls
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+        public NavigationMenuViewComponent(IStoreRepository repo) 
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke() 
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
